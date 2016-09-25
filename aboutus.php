13116819d05d88620fad34d9c256bee7616feaa3
subhadeepmukherjee <subhadeep.mukherjee@outlook.com>
2016-09-25 21:46:17 +0530

    Subhadeep:changes form arka received.

--- aboutus.php
+++ aboutus.php
@@ -19,12 +19,29 @@
     	<!-- **Content Full Width** -->
     	<div class="content content-full-width">
 		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
-			 <h1 class="title"> <span> About Ameera </span> </h1>
-			 <p><b>AMEERA</b> is one of the leading service providers of unisex beauty salons. We are one of the reputaed service provider with our strong 
+                <img src="images/about.png">
+		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                
+                <h3>  <p><b>AMEERA</b> is one of the leading service providers of unisex beauty salons. We are one of the reputed service provider with our strong 
 			 presence in this industry. We are proud to declair, we can totally make over a person by our service in skin &amp; hair.</p>
-		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
-		<div style ="margin:0;padding:0;float:left:width:70%;"><img src="images/aboutus.jpg"></div>
-		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                </h3>
+                    <p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                    <br><br>
+                    <h5>
+                        <p><b>Ameera Glamour World</b> was started in the year 2002. Our journey began with the first branch 
+                            at Ho chi Minch Sarani. Since then we have added 8 more branches spread all over Kolkata. The Brand 
+                            has been associated with Loreal Professionel and caters to the clients of all age group. We have standard
+                            interior with good and relaxing chairs, shampoo stations, pedicure/manicure sections, separate facial rooms, 
+                            Bridal/make-up room and hair treatment zone.</p>
+                        
+                        <p>The staff are well trained and get training on regular basis to keep themselves upgraded with the latest trends. 
+                            The emphasis is to provide good service at affordable price. We maintain cleanliness and use disposable materials 
+                            wherever necessary.</p>
+                        <p>The products are of Loreal Professional for all sorts of hair styling and treatments. Skin care products are also 
+                            of reputed companies like Jansen-Germany, Aroma Magic, O3 and Vedic line.</p>
+                    
+                    </h5>
+                    <div style ="margin:0;padding:0;float:left:width:70%;"><img src="images/aboutus.jpg"></div>
         </div> <!-- **Content Full Width - End** -->   	
         <?php //require_once('parts/newsletter.php'); ?>
         
