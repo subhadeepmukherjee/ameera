b153bd624267213c93d4a1bdbd9da45f8b50b89d
subhadeepmukherjee <subhadeep.mukherjee@outlook.com>
2016-09-25 20:23:41 +0530

    Subhadeep:changes for arko and gallery page renamed

---
+++ Package.php
@@ -0,0 +1,36 @@
+<?php require_once('parts/head.php'); ?>
+<body class="home">
+<!-- **Header** -->
+<div id="header">
+	<div class="container">
+		<?php require_once('parts/header.php'); ?>
+        <!-- **Logo** -->
+        <div id="logo">
+        	<a href="index.php" title=""> <img src="images/logo.png" alt="" title="" height="100px" width="100px" /> </a>
+        </div><!-- **Logo - End** -->
+        <?php //require_once('parts/search.php') ?>
+    </div>
+</div><!-- **Header - End** -->
+<!-- ** Main** -->
+<div id="main" style="padding:0;margin:0;">
+	<!-- **Main Container** -->
+	<div class="main-container">
+    
+    	<!-- **Content Full Width** -->
+    	<div class="content content-full-width">
+		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                
+       <img src="images/offer.png">
+		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                <br>
+                <h2> 
+                    Just for few Days offer<br><br><br>
+                    1. One Detaning (Worth Rs.300) free with a Lotus Gold Facial<br>
+                     2. Shaving & Threading Free with a latest Haircut.
+</h2>
+                <img src="images/package.jpg">       
+        </div> <!-- **Content Full Width - End** -->   	
+        <?php //require_once('parts/newsletter.php'); ?>
+        
+    </div><!-- **Main Container - End** -->
+ <?php require_once('parts/footer.php'); ?>
