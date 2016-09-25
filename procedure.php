8da4500b60cd577bb466e85c2be3c94a0ae9cafa
subhadeepmukherjee <subhadeep.mukherjee@outlook.com>
2016-09-25 21:09:49 +0530

    Subhadeep:Populer procedure page developed by arka

---
+++ procedure.php
@@ -0,0 +1,152 @@
+<?php require_once('parts/head.php'); ?>
+<body class="home">
+<!-- **Header** -->
+<div id="header">
+	<div class="container">
+		<?php require_once('parts/header.php'); ?>
+        <!-- **Logo** -->
+        <div id="logo">
+        	<a href="index.php" title=""> <img src="images/logo.png" alt="" title="" height="100px" width="100px" /> </a>
+        </div><!-- **Logo - End** -->
+        <?php //require_once('parts/search.php') ?>
+    </div>
+</div><!-- **Header - End** -->
+<!-- ** Main** -->
+<div id="main" style="padding:0;margin:0;">
+	<!-- **Main Container** -->
+	<div class="main-container">
+    
+    	<!-- **Content Full Width** -->
+    	<div class="content content-full-width">
+		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                <img src="images/procedure.png"><br><br><br>
+                Our Popular services are given below
+		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
+                <br><br>
+                	<h1> Hair Styling</h1>
+            
+            <div class="column one-third">
+                <img src="images/blunt300.jpg" alt="" title="" />
+             <div class="box-content-with-design">
+                 <a href="booknow.php" class="big-ico-button red book"> <span> Book an Appointment </span> </a>                
+                    
+                <!-- 	<h3> Features of Ayurveda Spa </h3>
+                    <ul class="flower-bullet green">
+                        <li> Fusce nec purus dui, id placerat sem. </li>
+                        <li> Aenean at nunc sem, sit amet tristique nisi. </li>
+                        <li> Duis at neque in libero aliquam molestie. </li>
+                        <li> Nullam ornare ante et nulla accumsan molestie. </li>
+                        <li> Vivamus volutpat ultricies felis, at sodales. </li>
+                        <li> Nam faucibus nisi et nisl suscipit suscipit. </li>
+                        <li> Sed non metus quam, id tincidunt ligula. </li>
+                    </ul>
+                   <h3> Price Range </h3>
+                                        
+                    <table border="0" cellpadding="0" cellspacing="0" class="price-table">
+                    	<tbody> 
+                        	<tr>
+                            	<td class="odd"> 1 hr </td>
+                                <td class="even"> $50 </td>
+                            </tr>
+                            <tr>
+                            	<td class="odd"> 2 hrs </td>
+                                <td class="even"> $75 </td>
+                            </tr>
+                            <tr>
+                            	<td class="odd"> 3 hrs (Max) </td>
+                                <td class="even"> $125 </td>
+                            </tr>
+                        </tbody>
+                    </table>  -->                  
+                </div>   
+            </div>
+                        <br>
+                        
+            
+            <div class="column two-third last">            
+            
+                	<h3> Our Top 7 Hair Styling services </h3>
+                    <ul class="flower-bullet green">
+                        <li> 1.	Step Haircut</li>
+                        <li> 2.	Layers Haircut</li>
+                        <li> 3.	Mushroom Haircut</li>
+                        <li> 4.	Volume Haircut </li>
+                        <li> 5.	Blunt Haircut</li>
+                        <li> 6.	Graduation Haircut </li>
+                        <li> 7.	Feather Haircut</li>
+                    </ul>               
+                        <p>we Provide Various types of Hair Styling according to your Face & Hair</p>
+                       
+ <!-- <blockquote> <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p> </blockquote>                
+       -->         
+       <div class="hr_invisible"> </div>          
+                     
+                    
+            </div>
+            
+            <div class="hr"> </div>
+            
+            <div class="clear"> </div>
+            
+            <h1> Gold Facial </h1>
+            
+            <div class="column one-third">
+                <img src="images/gold300.jpg" alt="" title="" />
+                <div class="box-content-with-design">
+                       <a href="booknow.php" class="big-ico-button red book"> <span> Book an Appointment </span> </a>   
+                <!--	<h3> Features of Ayurveda Spa </h3>
+                    <ul class="flower-bullet green">
+                        <li> Fusce nec purus dui, id placerat sem. </li>
+                        <li> Aenean at nunc sem, sit amet tristique nisi. </li>
+                        <li> Duis at neque in libero aliquam molestie. </li>
+                        <li> Nullam ornare ante et nulla accumsan molestie. </li>
+                        <li> Vivamus volutpat ultricies felis, at sodales. </li>
+                        <li> Nam faucibus nisi et nisl suscipit suscipit. </li>
+                        <li> Sed non metus quam, id tincidunt ligula. </li>
+                    </ul>
+                    <h3> Price Range </h3>
+                    <table border="0" cellpadding="0" cellspacing="0" class="price-table">
+                    	<tbody> 
+                        	<tr>
+                            	<td class="odd"> 1 hr </td>
+                                <td class="even"> $50 </td>
+                            </tr>
+                            <tr>
+                            	<td class="odd"> 2 hrs </td>
+                                <td class="even"> $75 </td>
+                            </tr>
+                            <tr>
+                            	<td class="odd"> 3 hrs (Max) </td>
+                                <td class="even"> $125 </td>
+                            </tr>
+                        </tbody>
+                    </table>    -->                
+                </div>
+            </div>
+            
+            <div class="column two-third last">            
+            	<h3> Top 7 Benefits of Gold facial </h3>
+                    <ul class="flower-bullet green">
+                        <li> 1.	Wrinkles, Fine Lines, Spots Can Be Reduced </li>
+                        <li> 2.	Stimulates The Skin Cells </li>
+                        <li> 3.	Premature Aging Of The Skin Can Be Prevented </li>
+                        <li> 4.	Gold Can Lighten The Complexion </li>
+                        <li> 5.	Collagen Depletion Is Slowed Down </li>
+                        <li> 6.	Sun Damage Can Be Treated </li>
+                        <li> 7.	Allergies Can Be Treated </li>
+                    </ul>               
+                        <p>Gold helps in improving the blood circulation, hence it helps hydrate and maintain the skin’s moisture level. The small particles of gold get absorbed into the skin imparting a rich glow. This makes the skin healthy, fresh and radiant.</p>
+                        <p>Gold not only gives a beautiful, glowing and youthful skin but is also beneficial in many ways. It slows down the collagen depletion, increases skin’s elasticity, lightens the skin’s complexion, stimulates the cells making the skin firm, improves blood circulation and prevents premature aging, wrinkles, tanning etc.</p>
+                        <p>To sum up, gold is gold. You will have to try it to believe the amazing gold skin care benefits.</p>
+          <!--      <blockquote> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </blockquote>                
+          -->   
+          <div class="hr_invisible"> </div>                
+                    
+                
+            </div>
+            
+        </div> <!-- **Content Full Width - End** -->   	
+        <?php //require_once('parts/newsletter.php'); ?>
+        
+    </div><!-- **Main Container - End** -->
+ <?php require_once('parts/footer.php'); ?>
