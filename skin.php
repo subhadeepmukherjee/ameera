a1a6a703d37221f31477cee2ddf21eb67422dc3a
subhadeepmukherjee <subhadeep.mukherjee@outlook.com>
2016-09-26 17:32:04 +0530

    Subhadeep:gallery pages updated

--- skin.php
+++ skin.php
@@ -19,12 +19,61 @@
     	<!-- **Content Full Width** -->
     	<div class="content content-full-width">
 		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
-                <img src="images/skin.png">
-		<p style="margin:0;padding:0;width:100%;">&nbsp;</p>
-                
-                
+		<script>
+            $(document).ready(function() {
+                $.museum($('#content img'));
+            });
+        </script>
+        <div id='content'>
+		<h1 class="title"> <span> Body </span> </h1>
+		<!-- Repeat this div for images-->
+		<div class="one-third herbal-sort">
+            <div class="gallery">
+                <div class="gallery-image">
+                <img src='http://muratayfer.com/static/images/paintings/cat.jpg' />
+				</div>
+				<div class="gallery-title">
+                    <h5> <a href="#" title=""> Misc Herbs </a> </h5>
+                </div>
+			</div>
+		</div>
+		<!-- Repeat this div for images-->
+		<div class="one-third cosmetics-sort">
+            <div class="gallery">
+                <div class="gallery-image">
+                <img src='http://muratayfer.com/static/images/paintings/bird.jpg' />
+				</div>
+				<div class="gallery-title">
+                    <h5> <a href="#" title=""> Color in Disguise </a> </h5>
+                </div>
+			</div>
+		</div>
+		<div class="one-third treatment-sort">
+            <div class="gallery">
+                <div class="gallery-image">
+                <img src='http://muratayfer.com/static/images/paintings/horse.jpg' />
+			    </div>
+				<div class="gallery-title">
+                    <h5> <a href="#" title=""> Spa Bars </a> </h5>
+                </div> 
+			</div>
+		</div>
+		<div class="one-third health-sort">
+            <div class="gallery">
+                <div class="gallery-image">
+                <img src='http://muratayfer.com/static/images/paintings/snail.jpg' />
+				</div>
+				<div class="gallery-title">
+                    <h5> <a href="#" title=""> Honey Dew </a> </h5>
+                </div> 
+			</div>
+		</div>
+		
+     </div><!-- **content - End ** -->
+	 <p style="margin:0;padding:0;width:100%;">&nbsp;</p><br><br>
         </div> <!-- **Content Full Width - End** -->   	
         <?php //require_once('parts/newsletter.php'); ?>
         
     </div><!-- **Main Container - End** -->
- <?php require_once('parts/footer.php'); ?>
+<?php $include_footer_js = false; ?>
+<?php require_once('parts/footer.php'); ?>
