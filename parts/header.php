7e722ae4e5a7163fbed3a90f2eccbf586c0b7d87
subhadeepmukherjee <subhadeep.mukherjee@outlook.com>
2016-09-26 15:29:18 +0530

    Subhadeep:gallery structure changed.

--- parts/header.php
+++ parts/header.php
@@ -1,11 +1,20 @@
     <!-- **Top-Menu** -->
     	<div id="top-menu">
         	<ul class="menu">
-            	<li> <a href="index.php" title=""> <span> Its home </span> Home </a></li>                                          <li> <a href="aboutus.php" title=""> <span> Who we are </span> About Us </a></li>
+            	<li> <a href="index.php" title=""> <span> Its home </span> Home </a></li>
+				<li> <a href="aboutus.php" title=""> <span> Who we are </span> About Us </a></li>
                 <li> <a href="services.php" title=""> <span> Our Popular </span> Services </a></li>
                 <li> <a href="package.php" title=""> <span> Special offer </span> Packages </a></li>
 		<li> <a href="team.php" title=""> <span> Meet the </span> Team </a></li>
-		<li> <a href="gallery.php" title=""> <span> Visual look </span> Gallery </a></li>
+		<li> <a href="gallery.php" title=""> <span> Visual look </span> Gallery </a>
+                	<ul>
+                        <li><a href="hair.php" title=""> Hair Styling</a></li>
+                        <li><a href="skin.php" title=""> Skin Treatment</a></li>
+                        <li><a href="body.php" title=""> Body Care </a></li>
+                        <li><a href="bridal.php" title=""> Bridal makeup</a></li>
+                    </ul>
+                
+                </li>
                 <li> <a href="booknow.php" title=""> <span> Why the wait </span> Book Now </a> </li>
                 <li> <a href="location.php" title=""> <span> We are here </span> Location </a> </li>
             </ul>        	
